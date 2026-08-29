1359c7ae26d86db7d1cfbc054d53492422b76949
ha kyeong <hess93hess93@gmail.com>
2026-08-29 20:44:29 +0900

    init

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      global: {
+        console: 'readonly',
+        precess: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      semi: ['error', 'always'],
+      queote: ['error', 'single'],
+    },
+  },
+];
